ace705dad02aa5156d5cbe0fd6477f6ef0d8ff6d
Peter Carrero <peter.carrero@gmail.com>
2015-08-27 14:34:26 -0500

    Add puppet and site.pp

--- setup.bash
+++ setup.bash
@@ -1,5 +1,6 @@
 #!/bin/bash
 rpm -Uvh https://yum.puppetlabs.com/puppetlabs-release-pc1-el-7.noarch.rpm
+yum install puppet hiera facter
 curl -s https://raw.githubusercontent.com/aloyr/system_config_files/master/setup.bash | GITEMAIL="username@gmail.com" bash
 sed -i.bak 's/PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/sshd_config
 systemctl restart sshd
---
+++ site.pp
@@ -0,0 +1,32 @@
+package { epel-release: ensure => 'installed' }
+$common_packages = [
+  'atop',
+  'curl',
+  'yum-cron',
+  'htop',
+  'iotop',
+  'iptraf',
+  'pv',
+  'rsyslog',
+  'screen',
+  'tcpdump',
+  'tmux',
+  'vim-enhanced',
+  'wget'
+]
+
+package { $common_packages:
+          ensure => 'installed',
+          require => Package ['epel-release']
+        }
+
+Exec {
+  path => [
+    '/usr/local/bin',
+    '/usr/local/sbin',
+    '/usr/bin',
+    '/usr/sbin',
+    '/bin',
+    '/sbin'
+  ],
+}

656205319f91d8680d6082fd91693a606abdedaf
Peter Carrero <peter.carrero@gmail.com>
2015-08-30 21:07:19 -0500

    Add initial puppet roll-out

--- setup.bash
+++ setup.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
-rpm -Uvh https://yum.puppetlabs.com/puppetlabs-release-pc1-el-7.noarch.rpm
-yum install puppet hiera facter
+rpm -Uvh -y https://yum.puppetlabs.com/puppetlabs-release-pc1-el-7.noarch.rpm
+yum install -y puppet-agent
 echo 'PATH="$PATH:/opt/puppetlabs/bin"' > /etc/profile.d/puppet.sh
 /opt/puppetlabs/bin/puppet module install saz-timezone
 /opt/puppetlabs/bin/puppet module install saz-sudo
@@ -18,3 +18,5 @@ iptables -A INPUT -m multiport -m tcp -p tcp --dports 22,80,443 -j ACCEPT
 iptables -A INPUT -i lo -j ACCEPT
 iptables -A INPUT -m limit --limit 6/min -j LOG --log-prefix "IPT: "
 iptables -A INPUT -j DROP
+site=$(curl https://raw.githubusercontent.com/aloyr/linode_setup/master/site.pp)
+/opt/puppetlabs/bin/puppet apply -e "$site"
